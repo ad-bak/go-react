2d7373ef30d56aa1e3c39c99b083c76d55eda216
Adnan <adbak.dev@gmail.com>
2024-01-28 04:06:53 +0100

    go

---
+++ back/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	var app application
+
+	app.Domain = "example.com"
+
+	log.Println("Starting server on port", port)
+
+	http.HandleFunc("/", Hello)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
