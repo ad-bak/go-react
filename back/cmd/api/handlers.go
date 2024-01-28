5cb00ba34396354d19fe78418706e600329072df
Adnan <adbak.dev@gmail.com>
2024-01-29 00:30:51 +0100

    cors

--- back/cmd/api/handlers.go
+++ back/cmd/api/handlers.go
@@ -1,10 +1,75 @@
 package main
 
 import (
+	"backend/internal/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go Movies up and running!",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []*models.Movie
+
+	rd, _ := time.Parse("2006-01-02", "1986-03-07")
+
+	highlander := models.Movie{
+		ID:          1,
+		Title:       "Highlander",
+		ReleaseDate: rd,
+		MPAARating:  "R",
+		Runtime:     116,
+		Description: "There can be only one",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	movies = append(movies, &highlander)
+
+	sicarioRd, _ := time.Parse("2006-01-02", "2015-09-18")
+
+	sicario := models.Movie{
+		ID:          2,
+		Title:       "Sicario",
+		ReleaseDate: sicarioRd,
+		MPAARating:  "R",
+		Runtime:     121,
+		Description: "An idealistic FBI agent is enlisted by a government task force to aid in the escalating war against drugs at the border area between the U.S. and Mexico.",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	movies = append(movies, &sicario)
+
+	out, err := json.Marshal(movies)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+
 }
